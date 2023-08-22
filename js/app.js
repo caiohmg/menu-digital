fc7488f7b1c8809598ddbbd0f72ca3d3ad01b133
caiohmg <108705024+caiohmg@users.noreply.github.com>
2023-08-21 22:08:16 -0300

    .

--- js/app.js
+++ js/app.js
@@ -52,30 +52,44 @@ cardapio.metodos = {
 
     $("#btnVerMais").addClass("hidden");
   },
+
+  // diminuir a quantidade de intens no cardapio
+  diminuirQuantidade: (id) => {
+    let qntdAtual = parseInt($("#qntd-" + id).text());
+
+    if (qntdAtual > 0) {
+      $("#qntd-" + id).text(qntdAtual - 1);
+    }
+  },
+
+  // aumentar a quantidade de intens no cardapio
+  aumentarQuantidade: (id) => {
+    let qntdAtual = parseInt($("#qntd-" + id).text());
+    $("#qntd-" + id).text(qntdAtual + 1);
+  },
 };
 
 cardapio.templates = {
   item: `
-
-      <div class="col-3 mb-5">
-      <div class="card card-item">
-          <div class="img-produto">
-              <img
-                  src="\${img}">
-          </div>
-          <p class="title-produto text-center mt-4">
-              <b>\${name}</b>
-          </p>
-          <p class="price-produto text-center">
-              <b>R$ \${price}</b>
-          </p>
-          <div class="add-carrinho">
-              <span class="btn-menos"><i class="fas fa-minus"></i></span>
-              <span class="add-numero-item">0</span>
-              <span class="btn-mais"><i class="fas fa-plus"></i></span>
-              <span class="btn btn-add"><i class="fa fa-shopping-bag"></i></span>
-          </div>
+    <div class="col-3 mb-5">
+      <div class="card card-item" id="\${id}">
+        <div class="img-produto">
+          <img src="\${img}">
+        </div>
+        <p class="title-produto text-center mt-4">
+          <b>\${name}</b>
+        </p>
+        <p class="price-produto text-center">
+          <b>R$ \${price}</b>
+        </p>
+        <div class="add-carrinho">
+          <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidade('\${id}')"><i class="fas fa-minus"></i></span>
+          <span class="add-numero-item" id="qntd-\${id}">0</span>
+          <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidade('\${id}')"><i class="fas fa-plus"></i></span>
+          <span class="btn btn-add"><i class="fa fa-shopping-bag"></i></span>
+        </div>
       </div>
-      </div>
-`,
+    </div>
+  `,
 };
+
